0a9ac21fa929bc870c05a2569aad417424f5bd6d
OctavioFiore <fioreoctavio6@gmail.com>
2026-06-18 20:55:55 -0300

    Agregando la exceptions

--- src/entities/DetallePedido.java
+++ src/entities/DetallePedido.java
@@ -1,5 +1,7 @@
 package entities;
+
 import exceptions.StockInvalidoException;
+import exceptions.ProductoInvalidoException;
 
 public class DetallePedido extends Base {
     private int cantidad;
@@ -8,12 +10,14 @@ public class DetallePedido extends Base {
 
     public DetallePedido(int cantidad, Producto producto) {
         if (cantidad <= 0) throw new StockInvalidoException("La cantidad debe ser mayor a 0.");
-        if (producto == null) throw new IllegalArgumentException("El producto no puede ser nulo.");
+
+        if (producto == null) throw new ProductoInvalidoException("El producto no puede ser nulo.");
+
+
         this.cantidad = cantidad;
         this.producto = producto;
         this.subtotal = cantidad * producto.getPrecio();
     }
-
     public int getCantidad() { return cantidad; }
     public Double getSubtotal() { return subtotal; }
     public Producto getProducto() { return producto; }
@@ -33,3 +37,4 @@ public class DetallePedido extends Base {
     }
 
 }
+
